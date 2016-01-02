fb94619804d3cf1de2efc14102395e8fbe1f5d24
eshou1014 <eshou1014@gmail.com>
2016-01-02 16:39:49 -0500

    Added tags and tests

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,16 @@
+require 'attribute_normalizer'
 class Tag < ActiveRecord::Base
+  normalize_attribute :name
+  before_save :to_downcase
+  before_save :replace_with_dashes
   has_and_belongs_to_many :questions
+  validates :name, uniqueness: true
+
+  def to_downcase
+    self.name = self.name.downcase
+  end
+
+  def replace_with_dashes
+    self.name = self.name.parameterize
+  end
 end
