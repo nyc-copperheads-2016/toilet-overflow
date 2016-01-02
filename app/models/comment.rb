c2c446998eec8c0fa1201d105b259d4aff1d9f92
James Kirkpatrick <jkirkpatrick1990@gmail.com>
2016-01-02 13:36:40 -0500

    adds validation to model. requires specs

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :commentable, polymorphic: true
   has_many :votes, as: :voteable
+
+  validates :content, presence: true, length: {minimum: 10}
+  validates :user_id, presence: true
 end
