2fd8bebc7f37fc6b7259479bc7c96b4ec343fda4
Arcangelo Petretta <arcpetretta@gmail.com>
2016-01-02 15:49:32 -0500

    Add  tests for question validations

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -10,7 +10,35 @@ RSpec.describe Question, type: :model do
 end
 
 describe Question do
+  it "should not allow questions without a title to be saved" do
+    q = Question.new(question_title:"", content:'123412341234123412341234123412341234')
+    q.save
+    expect(q.id.nil?).to eq(true)
+  end
+
+  it "should not allow questions without content to be saved" do
+    q = Question.new(question_title:"12341234123412341234", content:'123412341234')
+    q.save
+    expect(q.id.nil?).to eq(true)
+  end
+
+
   it "should require that a Question title be at least 10 characters" do
     q = Question.new(question_title: "123456", content: '123412341234123412341234123412341234')
     q.save
-    expect ()
+    expect(q.id.nil?).to eq(true)
+  end
+
+  it "should require that its content be at least 20 characters" do
+    q = Question.new(question_title: "123412341234123", content:"12345")
+    q.save
+    expect(q.id.nil?).to eq(true)
+  end
+
+  it "should belong to a user" do
+    u1 = User.new(username: "bob", password_digest: "123456789", email: "bob@bob.com")
+    u1.save
+    q = u1.questions.new(question_title: "What is the best programming language to learn if you have no previous experience?", content: "I would appreciate any advice that people have to offer. Thanks!")
+    expect(q.user_id).to eq(u1.id)
+  end
+end
