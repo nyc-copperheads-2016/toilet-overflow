a817ac7a8b0c4286ff3a558ab300e0e5998e47f0
James Kirkpatrick <jkirkpatrick1990@gmail.com>
2016-01-02 16:55:41 -0500

    adds belongs to user association

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :commentable, polymorphic: true
+  belongs_to :user
   has_many :votes, as: :voteable
 
   validates :content, presence: true, length: {minimum: 10}
