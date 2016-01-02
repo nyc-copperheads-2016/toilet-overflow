8b7d67d1410845cde9b620ddd47ef3611a8988a6
Jay Yee <fmo@opmbx.org>
2016-01-02 16:11:34 -0500

    Add validations for Answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,16 @@
 class Answer < ActiveRecord::Base
+  # before_save :default_chosen_false
+    
   has_and_belongs_to_many :editors, class_name: 'User'
   belongs_to :user
   belongs_to :question
-  has_many :comments, as: :commentable
-  has_many :votes, as: :voteable
+  has_many :comments, as: :commentable, dependent: :destroy
+  has_many :votes, as: :voteable, dependent: :destroy
+
+  validates :content, presence: true, length: { minimum: 10 }
+  validates :chosen, inclusion: { in: [true, false] }
+
+  def default_chosen_false
+    self.chosen = false
+  end
 end
