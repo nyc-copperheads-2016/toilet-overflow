1d3025799d093dff4de956f1e9804f35a0c595f9
James Kirkpatrick <jkirkpatrick1990@gmail.com>
2016-01-02 16:15:52 -0500

    attemt 3 for associations

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,19 +1,19 @@
 FactoryGirl.define do
   # in order to create a comment, we need content, a user associated with the comment, and the type of thing the comment is associated with. in this case question, or answer. we need to build the associated question or answer to attach this factory generated comment to it
-  factory :comment_for_question, class: "Comment" do
-    content Faker::Hacker.say_something_smart
-    association :user
-    association :commentable, :factory => :question
+  Factory.define :comment_for_question, class: Comment do |comment|
+    comment.content Faker::Hacker.say_something_smart
+    comment.association :user
+    comment.association :commentable, factory: :question
     # not sure on this line as for how we will use it in our rspec
-    commentable_type  "Question"
+    comment.commentable_type  "Question"
   end
 
-  factory :comment_for_answer, class "Comment" do
-    content Faker::Hacker.say_something_smart
-    association :user
-    association :commentable, :factory => :answer
+  Factory.define :comment_for_answer, class Comment do |comment|
+    comment.content Faker::Hacker.say_something_smart
+    comment.association :user
+    comment.association :commentable, factory: :answer
     # not sure on this line as for how we will use it in our rspec
-    commentable_type  "Answer"
+    comment.commentable_type  "Answer"
   end
 
 end
