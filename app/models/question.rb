58ee85b0bd55292439a4dad5f3485659c8e0439a
Jay Yee <fmo@opmbx.org>
2016-01-02 21:06:18 -0500

    Add user validations and tests

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,9 +2,9 @@ class Question < ActiveRecord::Base
   has_and_belongs_to_many :editors, class_name: 'User'
   belongs_to :user
   has_and_belongs_to_many :tags
-  has_many :answers
-  has_many :comments, as: :commentable
-  has_many :votes, as: :voteable
+  has_many :answers, dependent: :destroy
+  has_many :comments, as: :commentable, dependent: :destroy
+  has_many :votes, as: :voteable, dependent: :destroy
 
   validates :question_title, :content, :user_id, presence: true
   validates :question_title, length: {minimum: 10}
