d81d848b48c5762a7b194d686ace9d547b029ef8
Arcangelo Petretta <arcpetretta@gmail.com>
2016-01-03 14:54:48 -0500

    Add validations for votes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,6 @@
 class Vote < ActiveRecord::Base
   belongs_to :voteable, polymorphic: true
 
-  # validates :user_id, :uniqueness => {:scope => [:voteable_id, :voteable_type]}
+  validates :user_id, :uniqueness => {:scope => [:voteable_id, :voteable_type]}
   validates :vote, :inclusion => {:in => [-1,1]}
 end
