83dc3537410378914cd591a54c0140bd0879adc3
Jay Yee <fmo@opmbx.org>
2016-01-04 02:51:28 -0500

    Make significant edits to logic and refactoring

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,18 +9,18 @@ u = User.create(username: "jay", password_digest: "hi", email: "jay@jay.com")
     q = FactoryGirl.create(:question, user_id: u.id)
 
     3.times do
-      q.tags.create(name: Faker::Lorem.word)
+      q.tags.create(name: Faker::Lorem.characters(5))
     end
 
     4.times do
-      a = q.answers.create(content: Faker::Lorem.sentences(3).join(" "), user_id: User.last.id)
+      a = q.answers.create(content: Faker::Hipster.paragraph, user_id: User.last.id)
       3.times do
-        a.comments.create(content: Faker::Lorem.sentences(1).join(" "), user_id: User.last.id)
+        a.comments.create(content: Faker::Hipster.sentences(1).join(" "), user_id: User.last.id)
       end
     end
 
     2.times do
-      q.comments.create(content: Faker::Lorem.sentences(1).join(" "), user_id: User.last.id)
+      q.comments.create(content: Faker::Hacker.say_something_smart, user_id: User.last.id)
     end
 
   end
